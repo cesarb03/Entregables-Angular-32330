42056ee636f81d1781cc825d8953b4dca31b92f6
cesarb03 <cesar_b_03@outlook.com>
2022-12-01 18:27:51 -0300

    Clase 4 - Entregable 2 - Lista de Alumnos

--- Entregable - Clase 4 - Lista de alumnos/src/app/app.module.ts
+++ Entregable - Clase 4 - Lista de alumnos/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentComponent } from './components/content/content.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    AlumnosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Entregable - Clase 4 - Lista de alumnos/src/app/components/alumnos/alumnos.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../../models/students';
+
+@Component({
+  selector: 'app-alumnos',
+  templateUrl: './alumnos.component.html',
+  styleUrls: ['./alumnos.component.css']
+})
+export class AlumnosComponent implements OnInit {
+  students: Student[] =  [
+    new Student(1,'Lionel', 'Messi', new Date ('1987-06-24'), true),
+    new Student(2,'Lionel', 'Messi', new Date ('1987-06-24'), false),
+    new Student(3,'Lionel', 'Messi', new Date ('1987-06-24'), true),
+    new Student(4,'Lionel', 'Messi', new Date ('1987-06-24'), true),
+    new Student(5, 'Lionel', 'Messi', new Date ('1987-06-24'), false),
+    new Student(6, 'Lionel', 'Messi', new Date ('1987-06-24'), true),
+    new Student(7, 'Lionel', 'Messi', new Date ('1987-06-24'), true),
+    new Student(8, 'Lionel', 'Messi', new Date ('1987-06-24'), true),
+    new Student(9, 'Lionel', 'Messi', new Date ('1987-06-24'), true)
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
